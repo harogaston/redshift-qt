c98b1443d748f989d39730c6d41bccea1d9677ac
Gastón Haro <gaston.haro@fing.edu.uy>
2017-07-20 21:52:37 -0300

    Tray Icon Context Menu
    Tray icon context menu and submenus done (GUI only).

--- redshift-qt/widget.cpp
+++ redshift-qt/widget.cpp
@@ -1,14 +1,67 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include <QAction>
+#include <QCloseEvent>
+#include <QIcon>
+#include <QMenu>
+#include <QSystemTrayIcon>
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+
+    // Tray Icon Setup
+    trayIcon = new QSystemTrayIcon(this);
+    trayIcon->show();
+    connect(trayIcon, SIGNAL(activated(QSystemTrayIcon::ActivationReason)), this,
+            SLOT(trayIconActivated(QSystemTrayIcon::ActivationReason)));
+
+    QMenu *trayIconMenu = new QMenu(this);
+
+    QAction *enableAction = new QAction(tr("&Enable"), this);
+    enableAction->setCheckable(true);
+    trayIconMenu->addAction(enableAction);
+
+    QMenu *suspendMenu = new QMenu("&Suspend for", trayIconMenu);
+    QAction *suspend30MinAction = new QAction(tr("&30 minutes"), this);
+    QAction *suspend1HourAction = new QAction(tr("&1 hour"), this);
+    QAction *suspend2HoursAction = new QAction(tr("&2 hours"), this);
+    suspendMenu->addAction(suspend30MinAction);
+    suspendMenu->addAction(suspend1HourAction);
+    suspendMenu->addAction(suspend2HoursAction);
+    trayIconMenu->addMenu(suspendMenu);
+
+    QAction *autostartAction = new QAction(tr("&Autostart"), this);
+    autostartAction->setCheckable(true);
+    trayIconMenu->addAction(autostartAction);
+
+    QAction *infoAction = new QAction(tr("&Info"), this);
+    trayIconMenu->addAction(infoAction);
+
+    QAction *quitAction = new QAction(tr("&Quit"), this);
+    connect(quitAction, SIGNAL(triggered()), qApp, SLOT(quit()));
+    trayIconMenu->addAction(quitAction);
+
+    trayIcon->setContextMenu(trayIconMenu);
 }
 
 Widget::~Widget()
 {
     delete ui;
+}
+
+void Widget::closeButtonClicked()
+{
+    hide();
+}
+
+void Widget::closeEvent(QCloseEvent *event) {
+    hide();
+    event->ignore();
+}
+
+void Widget::trayIconActivated(QSystemTrayIcon::ActivationReason reason) {
+  Widget::showNormal();
 }
--- redshift-qt/widget.h
+++ redshift-qt/widget.h
@@ -1,22 +1,30 @@
 #ifndef WIDGET_H
 #define WIDGET_H
 
+#include <QSystemTrayIcon>
 #include <QWidget>
 
 namespace Ui {
 class Widget;
 }
 
-class Widget : public QWidget
-{
-    Q_OBJECT
+class Widget : public QWidget {
+  Q_OBJECT
 
 public:
-    explicit Widget(QWidget *parent = 0);
-    ~Widget();
+  explicit Widget(QWidget *parent = 0);
+  ~Widget();
+
+protected:
+  void closeEvent(QCloseEvent *event);
+
+private slots:
+  void closeButtonClicked();
+  void trayIconActivated(QSystemTrayIcon::ActivationReason reason);
 
 private:
-    Ui::Widget *ui;
+  Ui::Widget *ui;
+  QSystemTrayIcon *trayIcon;
 };
 
 #endif // WIDGET_H
